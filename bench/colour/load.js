a01c57b2f19f88062c9b0736ee021860b5077a18
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2019-07-20 19:37:04 +0100

    Add benchmark from kleur

---
+++ bench/colour/load.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+
+console.time( "chalk" );
+const chalk = require( "chalk" );
+console.timeEnd( "chalk" );
+
+console.time( "kleur" );
+const kleur = require( "kleur" );
+console.timeEnd( "kleur" );
+
+console.time( "ansi-colors" );
+const color = require( "ansi-colors" );
+console.timeEnd( "ansi-colors" );
+
+console.time( "colourant" );
+const { colourant } = require( "../.." );
+console.timeEnd( "colourant" );
